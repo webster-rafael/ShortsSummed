c3b14b68e2850aadbc5887c9fd81a4ee4b5bec1d
Webster Dev <webster.rafael2014@gmail.com>
2023-09-15 05:37:09 -0300

    finish

---
+++ main.js
@@ -0,0 +1,8 @@
+// JS
+import "./web/form.js"
+
+// CSS
+import "./web/styles/base.css"
+import "./web/styles/app.css"
+import "./web/styles/form.css"
+import "./web/styles/footer.css"
--- web/main.js
+++
@@ -1,8 +0,0 @@
-// JS
-import "./form.js"
-
-// CSS
-import "./styles/base.css"
-import "./styles/app.css"
-import "./styles/form.css"
-import "./styles/footer.css"
